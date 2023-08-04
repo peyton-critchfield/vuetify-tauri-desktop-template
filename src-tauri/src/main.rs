716170b796e8a2aa8baa00bb9e155cda68c25621
peyton-critchfield <pc.sefl@gmail.com>
2023-08-04 09:42:16 -0400

    init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn greeting() -> String {
+  let response: String = String::from("Greeting from Rust");
+  response
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greeting])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,149 +1,57 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
-        </h1>
-
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="container">
+    <div style="display: flex">
+      <v-img
+      :src="require('../assets/logo.svg')"
+      class="my-3"
+      contain
+      height="200"
+      />
+      <v-img
+      :src="require('../assets/logo.png')"
+      class="my-3"
+      contain
+      height="200"
+      />
+    </div>
+    <div style="display: flex">
+      <v-img
+      :src="require('../assets/tauri.png')"
+      class="my-3"
+      contain
+      height="200"
+      />
+      <v-img
+      :src="require('../assets/rust.png')"
+      class="my-3"
+      contain
+      height="200"
+      />
+    </div>
+    <div v-if="greeting" style="color:white;text-align: center;">
+      {{ greeting }}
+    </div>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from "vue";
+import { invoke } from '@tauri-apps/api/tauri'
 
-export default {
-  name: 'HelloWorld',
+const greeting = ref("");
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+const init = async () => {
+    greeting.value = await invoke("greeting");
+    console.log(greeting.value)
 }
+init()
+
 </script>
+
+<style>
+.container {
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
